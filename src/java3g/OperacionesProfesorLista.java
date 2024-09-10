505b54982a39982f7f8bf931337412b9ab270732
Mario Moreno <mariormz246@gmail.com>
2024-09-10 11:58:02 -0600

    Crear una clase Profesor con sus atributos, getters y setters para aplicar encapsulamiento y pasar los datos de la interfaz al CRUD

---
+++ src/java3g/OperacionesProfesorLista.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java3g;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author mariormoreno
+ */
+public class OperacionesProfesorLista extends CRUD{
+    
+     ArrayList<Profesor> objListaProfesor;
+     Profesor objProfesor; // crear un objeto Estudiante que se use para aplicar polimorfismo en la clase CRUD ya que cuenta actualmente con dos métodos (Estudiante y Profesor)
+
+    public OperacionesProfesorLista() {
+        objListaProfesor = new ArrayList();
+    }
+
+    @Override
+    public void create() {
+        objListaProfesor.add(objProfesor);
+    }
+
+    @Override
+    public void read() {
+    }
+
+    @Override
+    public void update() {
+    }
+
+    @Override
+    public void delete() {
+    }
+    
+    public Profesor getObjProfesor() {
+        return objProfesor;
+    }
+
+    public void setObjProfesor(Profesor objProfesor) {
+        this.objProfesor = objProfesor;
+    }
+
+    
+}
