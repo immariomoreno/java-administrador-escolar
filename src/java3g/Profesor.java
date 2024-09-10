63def3a75a0d1c11e997965b09495eecb6d833a1
Mario Moreno <mariormz246@gmail.com>
2024-09-10 12:16:27 -0600

    Crear una clase Profesor con sus atributos, getters y setters para aplicar encapsulamiento y pasar los datos de la interfaz (ProfesorAlta) al CRUD en su método create() y así se cree un objeto Estudiante con todos sus atributos.

--- src/java3g/Profesor.java
+++ src/java3g/Profesor.java
@@ -10,5 +10,51 @@ package java3g;
  */
 public class Profesor {
     
+    String noPlaza;
+    String nombre;
+    String apellidoPaterno;
+    String apellidoMaterno;
+    int edad;
+
+    public String getNoPlaza() {
+        return noPlaza;
+    }
+
+    public void setNoPlaza(String noPlaza) {
+        this.noPlaza = noPlaza;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
     
 }
